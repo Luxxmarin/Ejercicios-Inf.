2bb12ec077ddebb3b9c53f7c71cdf9bf08c221d6
Luxxmarin <angelika.marin@udea.edu.co>
2026-08-20 11:33:26 -0500

    Add files via upload

---
+++ Ejercicios Inf_Clase virtual.py
@@ -0,0 +1,72 @@
+#Ejercicio 1
+#A = float(input("Ingrese el número A: "))
+#B = float(input("Ingrese el número B: "))
+#if A > B:
+#    print("{} es mayor que {}".format(A, B))
+#elif A < B:
+#    print("{} es menor que {}".format(A, B))
+#else:
+#    print("{} es igual a {}".format(A, B))
+
+#Ejercicio 2
+#a = int(input("Nota 1:"))
+#b = int(input("Nota 2:"))
+#c = int(input("Nota 3:"))
+#d = int(input("Nota 4:"))
+#e = int(input("Nota 5:"))
+#if a > 5:
+#    print("Nota 1 invalida, ingrese una nota valida:")
+#elif a < 0:
+#    print("Nota 1 invalida, ingrese una nota valida:")
+#if b > 5:
+#    print("Nota 2 invalida, ingrese una nota valida:")
+#elif b < 0:
+#    print("Nota 2 invalida, ingrese una nota valida:")
+#if c > 5:
+#    print("Nota 3 invalida, ingrese una nota valida:")
+#elif c < 0:
+#    print("Nota 3 invalida, ingrese una nota valida:")
+#if d > 5:
+#    print("Nota 4 invalida, ingrese una nota valida:")
+#elif d < 0:
+#    print("Nota 4 invalida, ingrese una nota valida:")
+#if e > 5:
+#    print("Nota 5 invalida, ingrese una nota valida:")
+#elif e < 0:
+#    print("Nota 5 invalida, ingrese una nota valida:")
+#    
+#promedio = (a*0.3) + (b*0.15) + (c*0.15) + (d*0.2) + (e*0.2)
+#if promedio >= 3:
+#    print("Estado: aprobado")
+#if promedio <= 2.9:
+#    print("Estado: reprobado")
+#
+#print("Promedio final: {}".format(promedio))
+
+#Ejercicio 3
+#Y = int(input("Escriba valor 1: "))
+#Z = int(input("Escriba valor 2: "))
+#if Y > Z:
+#    print("Valor X = 1")
+#elif Y == Z: 
+#    print("Valor X = 2")
+#else:
+#    print("Valor X = 3")
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
